52cb155fc8c512382fa707cf3456b7c391327b18
Cragjock <clark.steve@roadrunner.com>
2018-05-26 20:41:38 -0700

    SPI Cron for UTC

--- main.cpp
+++ main.cpp
@@ -3,9 +3,22 @@
 #include <thread>		//for chrono sleep this_thread::sleep_for(chrono::seconds(1));
 #include "mylcd.h"
 #include "SPI.h"
-
+#include <vector>
+#include "pitime.h"
 
 using namespace std;
+
+    // NIST
+    char NL = '\n';
+    char EL = '\0';
+    int k=0;
+    char str_filenm[33];
+
+    int (*device_open)(void);
+    //char* (*alt_pitime)();
+    int (*alt_pitime)(char*);
+    void get_NIST(char*);
+    // end NIST
 
     const string sdceng = {0x53,0x74,0x65,0x76,0x65,0x6e};
     //const char sdceng[] = {0x53,0x74,0x65,0x76,0x65,0x6e};        // ok
@@ -17,6 +30,7 @@ using namespace std;
     const char alpha[] ={"123456789a123456789b123456789c123456789d123456789e123456789f123456789g123456789h"};
 
     char buf[80];
+
 int main()
 {
 
@@ -28,9 +42,46 @@ int main()
     //myBus.SPI_write_reg(0x80);     // ==== TESTING
     //myBus.~SPIBus();
 
+    /// NIST
+    char* NISTbuffer;
+	NISTbuffer = new char[100];
+    buf_pitime(NISTbuffer);
+
+    vector<char> vecnist(100);
+	strcpy(&vecnist[0], NISTbuffer);	// STD reference page 278
+	vecnist.shrink_to_fit();
+
+    string mynist(NISTbuffer);
+	string line_1 = mynist.substr(7, 8);
+	string line_2 = mynist.substr(16, 8);
+
+	size_t found;
+	cout << line_1 << endl;
+	cout << line_2 << endl;
+	found = mynist.find("UTC");
+	string line_3 = mynist.substr(found, 9);
+	cout << line_3 << endl;
+	cout << mynist << endl;
 
 	lcddisplay theLCD;
-	this_thread::sleep_for(chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
+
+    theLCD.lcd_clear();
+
+ 	theLCD.lcd_set_cursor_address(0x00);
+	theLCD.lcd_write(line_3.c_str());
+	theLCD.lcd_write("Time");
+	theLCD.lcd_set_cursor_address(0x40);
+	theLCD.lcd_write(line_1.c_str());
+	theLCD.lcd_set_cursor_address(0x14);
+	theLCD.lcd_write(line_2.c_str());
+	theLCD.lcd_set_cursor_address(0x00);
+    sprintf(buf, "%c====== %c =======%c",LeftBM, MiddleBM,RightBM);
+    theLCD.lcd_write(buf);
+    /// end NIST
+
+
+	this_thread::sleep_for(chrono::seconds(3));
     theLCD<<"Hello\n";
 	theLCD.lcd_write(sdceng);
 	this_thread::sleep_for(chrono::seconds(1));
@@ -68,11 +119,6 @@ int main()
     theLCD<<bmp<<bmp1;
     theLCD<<LeftBM<<MiddleBM<<RightBM;
     this_thread::sleep_for(chrono::seconds(1));
-
-    //RS test = data;
-    //theLCD.LCD_send(test, 0x23);
-
-
 
     return 0;
 }
